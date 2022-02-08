66a6eebec8b43f44038667a53b6714611a9b327f
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-08 12:59:16 +0200

    Add dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create!(name: 'User1')
+
+apple = Food.create!(name: 'Apple', measurement_unit: 1, price: 2)
+pineapple = Food.create!(name: 'Pineapple', measurement_unit: 1, price: 4)
+chicken_breasts = Food.create!(name: 'Chicken Breasts', measurement_unit: 1, price: 6)
