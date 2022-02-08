ef1c1142964f77c2eacd61a66b49a169370e8196
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-08 23:48:34 +0200

    Configure cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods, foreign_key: 'user_id'
   has_many :recipes, foreign_key: 'user_id'
+
+  def admin?
+    role == 'admin'
+  end
 end
