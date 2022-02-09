a5d734e3a25f546363f3c94dea5552cffb0cf25f
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-09 13:45:42 +0200

    Configure cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return unless user.present?
-
-    if user.admin?
-      can :manage, :all
-    else
-      can :read, Recipe, public: true
-      can :create, [Food, Recipe], user_id: user.id
-      can :destroy, [Food, Recipe], user_id: user.id
-    end
+    can :manage, Food, user_id: user.id
+    can :manage, Recipe, user_id: user.id
+    can :read, :all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_204744) do
+ActiveRecord::Schema.define(version: 2022_02_09_112340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2022_02_08_204744) do
     t.datetime "confirmed_at", precision: 6
     t.datetime "confirmation_sent_at", precision: 6
     t.string "unconfirmed_email"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
