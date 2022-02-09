26f10a144b3c33acc40c1b845934644efbd01d43
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-09 23:26:31 +0200

    Implement the recipe_detals from the recipes show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,14 +5,18 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
+    @foods = Food.all
     # @recipe = @recipes.find(params[:id])
-    @user = User.find(params[:id])
-    @recipe = @user.recipes.find(params[:id])
+    # @user = User.find(params[:id])
+    # @recipe = @user.recipes.find(params[:id])
   end
 
   def new; end
 
-  def create; end
+  def create
+    redirect_to '/foods', notice: 'Success!'
+  end
 
   def destroy; end
 end
