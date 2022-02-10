3b04f2ef895f73f70031105ca7fe71a44002a3fc
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-10 11:49:46 +0200

    Implement the shopping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,5 +1,8 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @foods = Food.all
+    # @foods = Food.all
+    @foods = current_user.foods
+    # @total_food_items = @foods.size
+    # @total_price = @foods.sum('price')
   end
 end
