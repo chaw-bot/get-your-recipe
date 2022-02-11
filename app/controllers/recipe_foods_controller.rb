6c2ec7c0b6e13da2fe3afb764abbd204f247ee69
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-02-11 17:03:03 +0200

    Last commits

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -2,10 +2,11 @@ class RecipeFoodsController < ApplicationController
   def show; end
 
   def create
-    # @recipe = Recipe.find(params[:id])
-    @recipe = Recipe.where(id: :id)
+    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.where(id: :id)
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
     @recipe_food.user = current_user
+    @recipe_food.save!
     # @recipe = current_user.recipes.find(params[:id])
     # @recipe_foods = @recipe.recipe_foods.new(recipe_foods_params)
     # redirect_to recipe_path(@recipe.id), notice: 'Ingredient saved successfully'
@@ -17,7 +18,7 @@ class RecipeFoodsController < ApplicationController
   end
 
   private
-  
+
   def recipe_food_params
     params.require(:recipe_foods).permit(:quantity, :recipe_id)
   end
