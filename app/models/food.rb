ca5cecddef3a18c4328c52995cccf0be67944c2b
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-11 19:00:35 +0200

    Display recipe_food

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,6 @@ class Food < ApplicationRecord
   has_many :recipes, dependent: :destroy
 
   validates :name, presence: true
-  validates :measurement_unit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
