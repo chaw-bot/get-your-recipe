38749ffb53410258f06e4d3f564ca153644927d1
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-17 12:34:40 +0200

    remove confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -56,10 +56,6 @@ ActiveRecord::Schema.define(version: 2022_02_09_112340) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
-    t.string "confirmation_token"
-    t.datetime "confirmed_at", precision: 6
-    t.datetime "confirmation_sent_at", precision: 6
-    t.string "unconfirmed_email"
     t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
